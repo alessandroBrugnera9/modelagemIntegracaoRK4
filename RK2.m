e2521d76b3751ddaa81251e18274ad4d6511b273
alessandro <alessandro.brugnera@outlook.com>
2020-03-20 20:04:45 -0300

    RK2 check

--- RK2.m
+++ RK2.m
@@ -19,20 +19,19 @@ function [x1,v1,a1,x2,v2,a2] = RK2 (h,tFinal,m2,vel,fprop)
 		a2(i) = calcAceleracao2 ( x1(i), x2(i), v1(i), v2(i), a1(i), m2, vel, fprop);
 
 		%encontrando Ks
-		k1x1 = v1(i);
-		k1x2 = v2(i);
-		k1v1 = a1(i);
-		k1v2 = a2(i);
-		k2x1 = x1(i) + h*v1(i);
-		k2x2 = x2(i) + h*v2(i);
-		k2v1 = v1(i) + h*a1(i);
-		k2v2 = v2(i) + h*a2(i);
+		k1v1 = v1(i);
+		k1v2 = v2(i);
+		k1a1 = a1(i);
+		k1a2 = a2(i);
+		k2v1 = v1(i) + h*k1a1;
+		k2v2 = v2(i) + h*k1a2;
+		k2a1 = calcAceleracao1 ( x1(i) + h*v1(i), x2(i) + h*v2(i), v1(i) + h*a1(i), v2(i) + h*a2(i), m2, vel, fprop);
+		k2a2 = calcAceleracao2 ( x1(i) + h*v1(i), x2(i) + h*v2(i), v1(i) + h*a1(i), v2(i) + h*a2(i), k2a1, m2, vel, fprop);
 
-
-		v1(i+1) = v1(i) + h/2*(k1v1+k2v1);
-		v2(i+1) = v2(i) + h/2*(k1v2+k2v2);
-		x1(i+1) = x1(i) + h/2*(k1x1+k2x1);
-		x2(i+1) = x2(i) + h/2*(k1x2+k2x2);
+		v1(i+1) = v1(i) + h/2*(k1a1+k2a1);
+		v2(i+1) = v2(i) + h/2*(k1a2+k2a2);
+		x1(i+1) = x1(i) + h/2*(k1v1+k2v1);
+		x2(i+1) = x2(i) + h/2*(k1v2+k2v2);
 	end;
 	a1(passos+1) = calcAceleracao1 ( x1(passos+1), x2(passos+1), v1(passos+1), v2(passos+1), m2, vel, fprop);
 	a2(passos+1) = calcAceleracao2 ( x1(passos+1), x2(passos+1), v1(passos+1), v2(passos+1), a1(passos+1),m2, vel, fprop);
